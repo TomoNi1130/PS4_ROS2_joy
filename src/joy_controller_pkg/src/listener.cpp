6bfd4979b0d813d5275bef3d53cd7a2e54361d92
TomoNi11 <tomoki201130@gmail.com>
2025-02-01 01:34:28 +0900

    third

---
+++ src/joy_controller_pkg/src/listener.cpp
@@ -0,0 +1,56 @@
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+#include <sensor_msgs/msg/joy.hpp>
+#include <test_interface/msg/controller.hpp>
+
+using std::placeholders::_1;
+
+class Joy_Sub : public rclcpp::Node
+{
+public:
+    Joy_Sub()
+        : Node("Joy_Subscriver")
+    {
+        subscription_ =
+            this->create_subscription<sensor_msgs::msg::Joy>("joy", 10, std::bind(&Joy_Sub::topic_callback, this, _1));
+        publisher_ = this->create_publisher<test_interface::msg::Controller>("controller_data", 10);
+    }
+
+private:
+    void topic_callback(sensor_msgs::msg::Joy::UniquePtr msg)
+    {
+        auto message = test_interface::msg::Controller();
+        RCLCPP_INFO(this->get_logger(), "pub: '%d'", msg->buttons[0]);
+        message.lx = msg->axes[0];
+        message.ly = msg->axes[1];
+        message.rx = msg->axes[3];
+        message.ry = msg->axes[4];
+        message.buttons[0] = msg->buttons[0];
+        message.buttons[1] = msg->buttons[1];
+        message.buttons[2] = msg->buttons[2];
+        message.buttons[3] = msg->buttons[3];
+        message.buttons[4] = msg->buttons[4];
+        message.buttons[5] = msg->buttons[5];
+        message.buttons[6] = msg->buttons[6];
+        message.buttons[7] = msg->buttons[7];
+        message.buttons[8] = msg->buttons[8];
+        message.buttons[9] = msg->buttons[9];
+        message.buttons[10] = msg->buttons[10];
+        message.buttons[11] = msg->axes[2] - 1;
+        message.buttons[12] = msg->axes[5] - 1;
+        this->publisher_->publish(message);
+    }
+    rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscription_;
+    rclcpp::Publisher<test_interface::msg::Controller>::SharedPtr publisher_;
+};
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<Joy_Sub>());
+    rclcpp::shutdown();
+    return 0;
+}
